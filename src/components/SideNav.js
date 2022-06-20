154ee15617791bb2cff5d82600195da4ec7a3f4e
Felix <felixgarske@gmx.de>
2022-06-20 16:01:41 +0200

    heutigen Kalender anzeigen

---
+++ src/components/SideNav.js
@@ -0,0 +1,14 @@
+function SideNav(props) {
+    return (
+      <>
+        <aside>
+          <h2>Sidebar</h2>
+          <div>Ort</div>
+          <div>heute</div>
+          <div>morgen</div>
+          <div>Monat</div>
+        </aside>
+      </>
+    );
+  }
+  export default SideNav;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages
-import App from './App';
+import Calendar from './pages/Calendar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
@@ -12,13 +12,15 @@ import Signup from './pages/Signup';
 // components 
 import Header from "./components/Header";
 import Footer from './components/Footer';
+import SideNav from './components/SideNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Header />
+    <SideNav />
     <Routes>
-      <Route path="/" element={<App/>} />
+      <Route path="/" element={<Calendar/>} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
     </Routes>
