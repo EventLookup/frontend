c27fa9694f63653a8698ac1d3afff6c2d8715b12
Dietmar <eis0687.dietmar@gmail.com>
2022-06-21 16:25:36 +0200

    Dima: index.js merge conflict fixed

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages
-import Calendar from './pages/Calendar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Create from "./pages/Create";
+import Event from './pages/Event';
+import Calendar from './pages/Calendar';
 
 
 // components 
 import Header from "./components/header/Header";
 import Footer from './components/Footer';
-import SideNav from './components/SideNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Header />
-    <SideNav />
     <Routes>
       <Route path="/" element={<Calendar/>} />
       <Route path="/create" element={<Create/>} />
+      <Route path="/" element={<Event />} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
     </Routes>
