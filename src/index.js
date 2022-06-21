4ffc0bb31a57169af0eec9cac1a436677f016061
Felix <felixgarske@gmx.de>
2022-06-21 17:03:23 +0200

    felix-erstellt-index.css-version-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// design
+import "./index.css";
+
 // react stuff
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -8,17 +11,14 @@ import Calendar from './pages/Calendar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-
 // components 
 import Header from "./components/Header";
 import Footer from './components/Footer';
-import SideNav from './components/SideNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Header />
-    <SideNav />
     <Routes>
       <Route path="/" element={<Calendar/>} />
       <Route path="/login" element={<Login/>}/>
