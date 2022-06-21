3edd45b764570a98fd4255b0240b0633b43c58f3
Meryem <pinarbasi.hm@gmail.com>
2022-06-21 15:41:58 +0200

    single event view mobile first erstellt

--- src/index.js
+++ src/index.js
@@ -4,23 +4,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages
-import Calendar from './pages/Calendar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Event from './pages/Event';
 
 
 // components 
 import Header from "./components/Header";
 import Footer from './components/Footer';
-import SideNav from './components/SideNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Header />
-    <SideNav />
     <Routes>
-      <Route path="/" element={<Calendar/>} />
+      <Route path="/" element={<Event />} />
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
     </Routes>
