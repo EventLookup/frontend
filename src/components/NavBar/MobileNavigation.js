0549ef0ae03b0810c200facc5975461e3f1aa44c
Jerry T <89188588+Aypoleyth@users.noreply.github.com>
2022-06-29 10:47:47 +0200

    Jerry: close burgermenu on navlink click (#18)

--- src/components/NavBar/MobileNavigation.js
+++ src/components/NavBar/MobileNavigation.js
@@ -2,14 +2,14 @@ import NavLinks from "./NavLinks";
 import "./NavBar.css"
 import { TbMenu2 } from 'react-icons/tb';
 import {CgClose} from "react-icons/cg";
-
-import { useState } from "react";
+import { useContext } from "react";
+import { MenuContext } from "../../context/MobileNavBarContext";
 
 const MobileNavigation = () => {
-const [open, setOpen] = useState(false);
+const {open, setOpen} = useContext(MenuContext);
 
-const hamburgerIcon = <TbMenu2 onClick={() => setOpen(!open)} className="hamburger"/>;
-const closeIcon = <CgClose onClick={() => setOpen(!open)} className="hamburger"/>;
+const hamburgerIcon = <TbMenu2 onClick={() => setOpen(true)} className="hamburger"/>;
+const closeIcon = <CgClose onClick={() => setOpen(false)} className="hamburger"/>;
 
 return(
         <nav className="mobile-nav">
