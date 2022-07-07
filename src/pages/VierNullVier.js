c7efc6cba1c5682ee9df8b87cb51ebc3a26d8fc0
Janina Spille <janina.spille@gmail.com>
2022-07-07 12:06:14 +0200

    Janina: 404 seite erstellt

---
+++ src/pages/VierNullVier.js
@@ -0,0 +1,14 @@
+import "./VierNullVier.css"
+
+const VierNullVier = () =>{
+    return (
+        <div className="vier-null-vier">
+            <main>
+                <h1>404</h1>
+                <h2>Page Not Found</h2>
+            </main>
+        </div>
+    )
+}
+
+export default VierNullVier
