7fa54f17cd877b31feeba51335e57ad45236a495
MeryemPin <89196669+MeryemPin@users.noreply.github.com>
2022-07-13 15:33:31 +0200

    Meryem: erstellt signup view (mobile first)
    * frontend fehler meldungen gefixt
    
    * zwischenstand
    
    * inputs für adresse erstellt
    
    * aktueller stand
    
    * aktueller stand
    
    * aktuell
    
    * sign up errors fixed
    
    * navigate done
    
    * signup endlich fertig

--- src/context/LoginAuthContext.js
+++ src/context/LoginAuthContext.js
@@ -1,9 +1,9 @@
 import { useState , createContext } from "react";
 import axios from 'axios';
 
-export const LoginAuthContext = createContext();
+export const AuthContext = createContext();
 
-export const LoginAuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({children}) => {
     const [ token , setToken ] = useState('');
     const [loggedIn , setLoggedIn] = useState(false);
     // console.log("CONTEXT token " , token);
@@ -12,8 +12,9 @@ export const LoginAuthContextProvider = ({children}) => {
 
     // console.log('context', axios.defaults.headers.common.authorization);
     return (
+
         <LoginAuthContext.Provider value={{token , setToken , loggedIn , setLoggedIn}}>
             {children}
-        </LoginAuthContext.Provider>
+        </AuthContext.Provider>
     )
 }
